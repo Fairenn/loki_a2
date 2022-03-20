ceef64a3ebbca5d29b989cbd42466dad0c98d71b
LaraBusyanil <larabusyanil0449@gmail.com>
2022-03-20 23:13:17 +0700

    selesaii

---
+++ 16_19.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const app = express()
+
+app.get("/loki_b2", function(req,res){ 
+res.send("berhasil di tampilkan");
+
+});
+app.get("/16", function(req,res){ 
+    res.send("berhasil diubah");
+    
+});
+
+app.get("/17", function(req,res){ 
+    res.send("berhasil dihapus");
+    
+});
+
+app.get("/18", function(req,res){ 
+    res.send("berhasil ditambah");
+    
+});
+
+app.get("/19", function(req,res){ 
+    res.send("berhasil diubah");
+    
+});
+app.listen(3000, function(){
+    console.log("Server aktif di port 3000")
+});
