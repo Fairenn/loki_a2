58cf7673d0ec42094def1753c435a8a70efb9a31
ruthemerald <ruthemeralda07@gmail.com>
2022-03-27 14:14:48 +0700

    yey

--- Referensi.js
+++ Referensi.js
@@ -3,43 +3,44 @@ const router = express.Router();
 
 //Menambah referensi
 router.post("/addref", (req, res) => {
-    let obj={
-        "message":"Silahkan tambah referensi disini",
-        "id":101,
-        "nama":[
-            "Bovee & Thill (2009:61)",
-            "Renn (2003:95 )"
-        ]
+    let add={
+        "message":"Tambah referensi di sini",
+        "id":112,
+        "pengarang":[
+            "Matt Doyle (2011)"
+        ],
+        "judul":[
+            "Beginning PHP 5.3"
+        ], 
     }
-    res.json(obj);
+    res.json(add);
     console.log("Referensi berhasil ditambahkan");
 });
   
-  //route menghapus referensi
+  //Menghapus referensi
   router.delete("/deleteref/:id", (req, res) => {
     let obj={
         "message":"Masukkan id referensi yang akan dihapus",
-        "id":102,
-        "nama":[
-            "Peter (2006:01)",
-        ]
+        "id":111,
     }
     res.json(obj);
-    console.log("Referensi dengan Id"+req.params.id+" berhasil dihapus");
+    console.log("Referensi dengan Id"+req.params.id+" telah berhasil dihapus");
   });
   
-  //route mengubah referensi
+  //Mengubah referensi
   router.put("/updateref", (req, res) => {
     let upd={
-        "message":"Masukkan id referensi yang akan diUpdate/ dimodifikasi",
-        "id":101,
+        "message":"Masukkan id referensi yang akan diubah",
+        "id":112,
         "nama":[
-            "Bovee & Thill (2009:61)",
-            "Renn (2003:95 )"
-        ]
+            "Matt Doyle (2009)"
+        ],
+        "judul": [
+            "Beginning PHP 5.3"
+        ],
     }
     res.json(upd);
-    console.log("Referensi berhasil diupdate/dimodifikasi");
+    console.log("Referensi dengan Id"+req.params.id+" telah berhasil diubah");
   });
   
   module.exports = router;
