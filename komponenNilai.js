e4f41bddaa831a392f08a8a5a0341e5540991de2
LaraBusyanil <larabusyanil0449@gmail.com>
2022-03-28 07:03:37 +0700

    commit

--- komponenNilai.js
+++ komponenNilai.js
@@ -3,14 +3,34 @@ const router = express.Router();
 
   //route Dosen dapat mengubah komponen penilaian
   router.put("/Ubah", (req, res) => {
-
+    let upd={
+      "message":"-----Ubah Komponen Nilai-----",
+      "Course_id":"JSI62125",
+      "Name":"Pemograman Web",
+      "komponen_nilai":[ "1. Tugas   = 10%",
+                         "2. Kuis    = 15%",
+                         "3. UTS     = 35%",
+                         "4. UAS     = 35%",
+                         "5. Sikap    = 5%"
+       ]
+    }
     res.json(del);
-    console.log("Komponen penilaian berhasil ditambahkan");
+    console.log("Komponen penilaian berhasil diubah");
   });
   
   //route Dosen dapat menghapus komponen penilaian
   router.delete("/hapus", (req, res) => {
-   
+    let del={
+      "message":"-----Hapus Komponen Nilai-----",
+      "Course_id":"JSI62125",
+      "Name":"Pemograman Web",
+      "komponen":[
+        "1. Tugas   = 10%",
+        "2. Kuis    = 15%",
+        "3. UTS     = 35%",
+        "4. UAS     = 35%",
+       ]
+    }
     res.json(del);
     console.log("Komponen penilaian berhasil dihapus");
   });
