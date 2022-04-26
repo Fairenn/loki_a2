d269b7792565e21ee59dc94007aef0ea32fb9c86
Fairenn <faizabdullah2708@gmail.com>
2022-04-26 11:23:22 +0700

    coba mengambil data dari database loki

--- databaseLoki.js
+++ databaseLoki.js
@@ -1,16 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const mysql = require('mysql2');
+// const express = require("express");
+// const router = express.Router();
+// const mysql = require('mysql2');
 
-router.get('/Loki', function(req, res, next){
+// router.get('/Loki', function(req, res, next){
 
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password:"",
-        database:"loki" });
-    connection.connect();
-});
+//     const connection = mysql.createConnection({
+//         host: "localhost",
+//         user: "root",
+//         password:"",
+//         database:"loki" });
+//     connection.connect();
+// });
 
-console.log("Berhasil Connect ke Database")
-module.exports = router;
+// console.log("Berhasil Connect ke Database")
+// module.exports = router;
+
+const { Sequelize } = require('sequelize');
+
+const databaseLoki = new Sequelize('loki', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+  });
+
+  try {
+    databaseLoki.authenticate();
+    console.log('Connection has been established successfully.');   //tes koneksi
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+module.exports = databaseLoki;
+
--- server.js
+++ server.js
@@ -2,13 +2,25 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const controller = require(`./controller/controllerUsers`);
+
 
 const komponenNilai = require('./komponenNilai');
 app.use('/komponenNilai',komponenNilai)
 
-const databaseLoki = require('./databaseLoki');
-app.use('/databaseLoki',databaseLoki)
+// const databaseLoki = require('./databaseLoki');
+// app.use('/databaseLoki',databaseLoki)
 
+const RPS = require('./RPS');
+app.use('/RPS',RPS)
+
+const CPMK = require('./CPMK');
+app.use('/CPMK',CPMK)
+
+const referensi = require('./referensi');
+app.use('/referensi',referensi)
+
+app.get("/users", controller.retrieveAll);
 
 app.get("/cetak", (req, res) =>       
 {
