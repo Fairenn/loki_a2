e0a34a194f5dde6931f956f16d67c5876a4cff0b
Fairenn <faizabdullah2708@gmail.com>
2022-05-26 13:17:56 +0700

    edit views ejs

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const router = express.Router()
+const router = express()
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+
+router.set('view engine', 'ejs');
+router.use( express.static( "views" ) );
 
 router.post("/login", (req, res) => 
 {
@@ -13,4 +16,10 @@ router.get("/logout", (req, res) =>
     res.send("Ini adalah Halaman logout");
 });
 
+router.get("/login", (req, res) => 
+{
+    res.render('login');
+});
+
 module.exports = router;
+
--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const controller = require(`./controller/controllerUsers`);
 
 
 const komponenNilai = require('./komponenNilai');
+
+app.set('view engine', 'ejs');
+app.use( express.static( "views" ) );
+
 app.use('/komponenNilai',komponenNilai)
 
 // const databaseLoki = require('./databaseLoki');
 // app.use('/databaseLoki',databaseLoki)
+
+const auth = require('./auth');
+app.use('/auth',auth)
 
 const RPS = require('./RPS');
 app.use('/RPS',RPS)
@@ -27,6 +34,12 @@ app.get("/cetak", (req, res) =>
     res.send("Cetak RPS");
 });
 
+app.get("/", (req, res) =>       
+{
+    res.render('dashboard');
+});
+
 app.listen(port, () =>{
  console.log(`Server Sedang Berjalan`)
 });
+
